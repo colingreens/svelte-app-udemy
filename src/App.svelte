1d033aa27d6b45b9a53f7d1a555c29cda1b1168c
colin_greens <colin.j.benson@gmail.com>
2023-11-22 20:24:57 -0800

    todo list start

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,27 @@
 <script>
-  import Button from "./lib/Button.svelte";
+  import TodoList from "./lib/TodoList.svelte";
+  import {v4 as uuid} from "uuid"
+
+  const todos = [
+    {
+      id: uuid(),
+      title: "Todo 1",
+      completed: true
+    },
+    {
+      id: uuid(),
+      title: "Todo 2",
+      completed: false
+    },
+    {
+      id: uuid(),
+      title: "Todo 3",
+      completed: true
+    }
+  ]
 </script>
 
-<Button size="small" shadow bgColor="green" textColor="white">Button Text</Button>
+<TodoList {todos}/>
 
 
 <style>
---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Button from "./Button.svelte";
+  import { v4 as uuid } from "uuid";
+
+  export let todos = [];
+  let input;
+  let inputText = "";
+
+  function handleAddTodo() {
+    if (!inputText) return;
+
+    // todos.push({
+    //   id: uuid(),
+    //   title: inputText,
+    //   completed: false,
+    // });
+
+    // todos = todos; //update state, push does not work, must use assignment operator.
+
+    todos = [
+      ...todos,
+      {
+        id: uuid(),
+        title: inputText,
+        completed: false,
+      },
+    ]; //or just do it this way
+
+    inputText = "";
+  }
+</script>
+
+<div class="todo-list-wrapper">
+  <ul>
+    {#each todos as { id, title }, index (id)}
+      {@const number = index + 1}
+      <li>{number}-{title}</li>
+    {/each}
+  </ul>
+  <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+    <input bind:value={inputText} />
+    <Button type="submit" bgColor="red" disable={!inputText}>Add</Button>
+  </form>
+</div>
